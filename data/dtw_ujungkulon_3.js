1da8e41d395ec53970c8c9d23da82363c631737f
Rissa Rambe <71384953+rissarambe@users.noreply.github.com>
2021-09-17 16:01:47 +0700

    Add files via upload

---
+++ data/dtw_ujungkulon_3.js
@@ -0,0 +1,12 @@
+var json_dtw_ujungkulon_3 = {
+"type": "FeatureCollection",
+"name": "dtw_ujungkulon_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Pulau Peucang", "photo": "Pulau Peucang_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.261205312244698, -6.747454757692984 ] } },
+{ "type": "Feature", "properties": { "Name": "Karang Copong", "photo": "Karang Copong_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.2478308, -6.7262024 ] } },
+{ "type": "Feature", "properties": { "Name": "Ciapus", "photo": "Ciapus_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.2693978, -6.7367285 ] } },
+{ "type": "Feature", "properties": { "Name": "Padang Savana Cidaon", "photo": "Cidaon_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.2664543, -6.7617658 ] } },
+{ "type": "Feature", "properties": { "Name": "Sungai Cigenter", "photo": "Sungai Cigenter_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.4035718, -6.7491785 ] } }
+]
+}
---
+++ data/fasilitas_ujungkulon_2.js
@@ -0,0 +1,14 @@
+var json_fasilitas_ujungkulon_2 = {
+"type": "FeatureCollection",
+"name": "fasilitas_ujungkulon_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Pelabuhan Sumur" }, "geometry": { "type": "Point", "coordinates": [ 105.5837893, -6.6559559 ] } },
+{ "type": "Feature", "properties": { "Name": "Dermaga Pulau Peucang" }, "geometry": { "type": "Point", "coordinates": [ 105.263054307350473, -6.746319121787855 ] } },
+{ "type": "Feature", "properties": { "Name": "Dermaga Pulau Handeleum" }, "geometry": { "type": "Point", "coordinates": [ 105.4239, -6.754071 ] } },
+{ "type": "Feature", "properties": { "Name": "Mushala Pulau Peucang" }, "geometry": { "type": "Point", "coordinates": [ 105.263, -6.74536111111111 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU 34.4216" }, "geometry": { "type": "Point", "coordinates": [ 105.5853694, -6.6636668 ] } },
+{ "type": "Feature", "properties": { "Name": "Pos Resort Pulau Handeleum" }, "geometry": { "type": "Point", "coordinates": [ 105.423884, -6.753594 ] } },
+{ "type": "Feature", "properties": { "Name": "Dermaga Cidaon" }, "geometry": { "type": "Point", "coordinates": [ 105.264581522853291, -6.759266626520039 ] } }
+]
+}
